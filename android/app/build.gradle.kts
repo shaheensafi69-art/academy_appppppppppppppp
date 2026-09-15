da11ccd35c3bd1c50b6aab1bb021b8a80f0609e8
Shaheen Safi <ssafi9241@gmail.com>
2026-09-16 00:48:16 +0430

    fix: install lucide-react dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "org.safiacademy.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
